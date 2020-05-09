2b9eeb5542535727c8e7705721a18c8e47d5d9f6
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2020-05-10 04:57:46 +1000

    feat: post mapping

--- conference/src/main/java/com/mazum/conference/controller/UserController.java
+++ conference/src/main/java/com/mazum/conference/controller/UserController.java
@@ -2,6 +2,7 @@ package com.mazum.conference.controller;
 
 import com.mazum.conference.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,14 @@ public class UserController {
         user.setLastname(lastname);
         user.setAge(age);
 
+        return user;
+    }
+
+    @PostMapping("/user")
+    public User postUser(User user) {
+        System.out.println("User firstname:" + user.getFirstname());
+        System.out.println("User lastname:" + user.getLastname());
+        System.out.println("User age:" + user.getAge());
         return user;
     }
 }
