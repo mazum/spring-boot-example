8f4b48b900d64e990f30c371c67bde75da602846
Abhishek Mazumder <mazumder.abhishek@gmail.com>
2020-05-10 03:21:18 +1000

    feat: passing parameters

--- conference/src/main/java/com/mazum/conference/controller/RegistrationController.java
+++ conference/src/main/java/com/mazum/conference/controller/RegistrationController.java
@@ -1,15 +1,24 @@
 package com.mazum.conference.controller;
 
+import com.mazum.conference.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @GetMapping("registration")
-    public String getRegistration(Map<String, Object> model) {
-        
+    public String getRegistration(@ModelAttribute("registration")Registration registration) {
+
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String postRegistration(@ModelAttribute("registration")Registration registration) {
+        System.out.println("Registration: " + registration.getName());
         return "registration";
     }
 }
